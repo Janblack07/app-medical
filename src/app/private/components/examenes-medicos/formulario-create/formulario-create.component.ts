12d905e954bc67813a6dcdc78fc1b34254fc1459
Oliver-08-ojla <oliver2018es@gmail.com>
2023-08-11 14:14:03 -0500

    Componentes de examen medico y medicamentos listos

--- src/app/private/components/examenes-medicos/formulario-create/formulario-create.component.ts
+++ src/app/private/components/examenes-medicos/formulario-create/formulario-create.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-formulario-create',
   templateUrl: './formulario-create.component.html',
   styleUrls: ['./formulario-create.component.scss'],
 })
-export class FormularioCreateComponent  implements OnInit {
+export class FormularioCreateComponentExamenes  implements OnInit {
 
-  constructor() { }
+  FormExamenes!:FormGroup;
+
+
+  constructor(private formBuilder:FormBuilder) { 
+    this.FormExamenes = this.formBuilder.group({
+      nombre : new FormControl('',[Validators.required,Validators.minLength(3)]),
+      fecha : new FormControl('',Validators.required),
+      resultado : new FormControl('',Validators.required),
+      id : new FormControl('',[Validators.required,Validators.minLength(1)]),
+
+      
+    });
+  }
 
   ngOnInit() {}
+
+    
+
+    CreateExamenesMedicos(Form:any){
+      // Se muestra por consola los valores enviados desde el formulario
+      console.log(this.FormExamenes.value);
+    }
+  
 
 }
--- src/app/private/components/medicamentos/formulario-create/formulario-create.component.ts
+++ src/app/private/components/medicamentos/formulario-create/formulario-create.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-formulario-create',
   templateUrl: './formulario-create.component.html',
   styleUrls: ['./formulario-create.component.scss'],
 })
-export class FormularioCreateComponent  implements OnInit {
+export class FormularioCreateComponentMedicamentos  implements OnInit {
 
-  constructor() { }
+  FormMedicamentos!:FormGroup;
 
+  constructor(private formBuilder:FormBuilder) { 
+    this.FormMedicamentos = this.formBuilder.group({
+      nombre : new FormControl('',[Validators.required,Validators.minLength(3)]),
+      dosis : new FormControl('',[Validators.required,Validators.minLength(1)]),
+      lapso : new FormControl('',[Validators.required,Validators.minLength(3)]),
+      duracion : new FormControl('',[Validators.required,Validators.minLength(3)]),
+      id : new FormControl('',[Validators.required,Validators.minLength(1)]),
+      
+    });
+  }
   ngOnInit() {}
+
+  
+  CreateMedicamentos(Form:any){
+    // Se muestra por consola los valores enviados desde el formulario
+    console.log(this.FormMedicamentos.value);
+  }
 
 }
